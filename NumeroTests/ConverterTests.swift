f0c553f704f007bf58ed957774ced9b9b2b3e70a
yusuphjoluwasen <yusuphjoluwaseun@gmail.com>
2020-03-07 15:20:05 +0100

    added fastlane

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -44,6 +44,7 @@ class ConverterTests: XCTestCase {
   func testConversionForOne() {
     let result = converter.convert(1)
     XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+    
   }
 }
 
