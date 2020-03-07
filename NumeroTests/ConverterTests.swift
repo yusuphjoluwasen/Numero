ac1940ffcb772bfc35f7d1be569d1d7a88b1e3ca
yusuphjoluwasen <yusuphjoluwaseun@gmail.com>
2020-03-07 04:24:30 +0100

    added travis.yml and convertertest file

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -25,5 +25,26 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+  var converter: Converter!
+
+  override func setUp() {
+    super.setUp()
+    converter = Converter()
+  }
+
+  override func tearDown() {
+    converter = nil
+    super.tearDown()
+  }
+
+  func testConversionForOne() {
+    let result = converter.convert(1)
+    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+  }
+}
 
 
